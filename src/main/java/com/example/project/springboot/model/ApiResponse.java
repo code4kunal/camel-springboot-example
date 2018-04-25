141687286b8a501a3351480f10d0fe41171b3f24
Kunal Saxena <kunal1991@hotmail.com>
2018-04-26 00:30:17 +0530

    Added exception handler class

---
+++ src/main/java/com/example/project/springboot/model/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.example.project.springboot.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+  protected String status;
+}
---
+++ src/main/java/com/example/project/springboot/model/ApiResponseError.java
@@ -0,0 +1,22 @@
+package com.example.project.springboot.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponseError extends ApiResponse {
+  String code;
+  Object message;
+
+  public ApiResponseError() {
+    this.status = "error";
+  }
+
+  public ApiResponseError(Object error, String code) {
+    this.message = error;
+    this.code = code;
+    this.status = "error";
+  }
+}
