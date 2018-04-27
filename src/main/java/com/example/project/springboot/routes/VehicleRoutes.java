b9b1fb193c6b079fa6b6fa956f4bc2cd0f2405cd
akshay.vilekar <akshayvilekar@yahoo.com>
2018-04-27 08:52:46 +0530

    error

--- src/main/java/com/example/project/springboot/routes/VehicleRoutes.java
+++ src/main/java/com/example/project/springboot/routes/VehicleRoutes.java
@@ -13,6 +13,8 @@ import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
+import java.util.NoSuchElementException;
+
 @Component
 public class VehicleRoutes extends RouteBuilder {
 
@@ -23,51 +25,37 @@ public class VehicleRoutes extends RouteBuilder {
         // to use undertow component and run on port 8081
         .component("restlet").port(8081);
 
-
-    onException(VehicleNotFoundException.class)
-        .handled(true)
-        // use HTTP status 404 when data was not found
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(404))
-        .setBody(constant(""));
+    onException(NoSuchElementException.class)
+            .handled(true)
+            // use HTTP status 404 when data was not found
+            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(404))
+            .setBody(constant(""));
 
     onException(Exception.class)
         .handled(true)
         // use HTTP status 500 when we had a server side error
         .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
-        .setBody(simple("${exception.message}\n"));
+        .setBody(constant(""));
 
 
     rest("/v1")
         .get("/viewvehicles")
-        .responseMessage().code(200).message("List of vehicles").endResponseMessage()
-        .responseMessage().code(500).message("Server error").endResponseMessage()
         .to("direct:viewvehicles")
 
         .get("/viewvehicles/{id}").outType(Vehicle.class)
         .param().name("id").description("The vehicle id").endParam()
-        .responseMessage().code(200).message("The vehicle with the given id").endResponseMessage()
-        .responseMessage().code(404).message("Vehicle not found").endResponseMessage()
-        .responseMessage().code(500).message("Server error").endResponseMessage()
         .to("direct:veiwvehicle")
 
 
         .post("/addvehicle").type(Vehicle.class).outType(String.class)
-        .responseMessage().code(200).message("The created order id").endResponseMessage()
-        .responseMessage().code(400).message("Invalid input data").endResponseMessage()
-        .responseMessage().code(500).message("Server error").endResponseMessage()
         .to("direct:addvehicle")
 
 
         .put("/editvehicle/{id}").outType(Vehicle.class)
-        .responseMessage().code(200).message("The created order id").endResponseMessage()
-        .responseMessage().code(400).message("Invalid input data").endResponseMessage()
-        .responseMessage().code(500).message("Server error").endResponseMessage()
         .to("direct:editvehicle")
 
 
         .delete("/deletevehicle/{id}").outType(String.class)
-        .responseMessage().code(404).message("Vehicle not found").endResponseMessage()
-        .responseMessage().code(500).message("Server error").endResponseMessage()
         .to("direct:deletevehicle");
 
 
